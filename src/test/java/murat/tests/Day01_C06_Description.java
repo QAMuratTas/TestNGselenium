6f575e369ef62f23a8bc2b9270c4f2f6b059d701
QA Murat Tas <itmmt38@gmail.com>
2022-12-29 22:09:02 +0300

    testngFirst

---
+++ src/test/java/murat/tests/Day01_C06_Description.java
@@ -0,0 +1,27 @@
+package murat.tests;
+
+import org.testng.annotations.Test;
+
+public class Day01_C06_Description {
+
+
+    @Test(description = "Bu test sistemimizin login fonksiyonlarini kontrol eder")
+    public void loginTest(){
+
+    }
+
+    @Test(description = "Bu test sistemimizin Home Page fonksiyonlarini kontrol eder")
+    public void homePageTest(){
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
