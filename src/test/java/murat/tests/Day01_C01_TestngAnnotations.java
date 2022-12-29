e840e7f1126a22ce396a3e74859799ed1ca840b9
QA Murat Tas <itmmt38@gmail.com>
2022-12-29 20:57:09 +0300

    testng

---
+++ src/test/java/murat/tests/Day01_C01_TestngAnnotations.java
@@ -0,0 +1,27 @@
+package murat.tests;
+
+import org.testng.annotations.Test;
+
+public class Day01_C01_TestngAnnotations {
+    //@Before/After Suite
+    //@Before/After Test
+    //@Before/After Class
+    //@Before/After Method
+    //@Test
+
+    @Test
+    public void tes01(){
+
+
+
+    }
+
+
+    @Test
+    public void test02(){
+
+
+    }
+
+
+}
