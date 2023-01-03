b138856c7aa7a52c1dc86c0ec8663f582d5bb3a9
QA Murat Tas <itmmt38@gmail.com>
2023-01-03 21:18:52 +0300

    tuesday

---
+++ src/test/java/murat/tests/Day04_C02_ParallelTestAttribute.java
@@ -0,0 +1,18 @@
+package murat.tests;
+
+import org.testng.annotations.Test;
+
+public class Day04_C02_ParallelTestAttribute {
+
+    @Test(
+            threadPoolSize = 3,    // testimizin kac cekirdekte paralel olarak calisacagini belirler
+            invocationCount = 7    // testimizin kac defa calistiralacagini belirler
+    )
+    public void test(){
+
+        System.out.println("Current thread Id = " + Thread.currentThread().getId());
+
+    }
+
+
+}
