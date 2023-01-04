0d4f33b47eb33dc352169c33d4409b1fd4904541
QA Murat Tas <itmmt38@gmail.com>
2023-01-04 20:41:18 +0300

    wed

---
+++ src/test/java/murat/utilities/BaseTestReport.java
@@ -0,0 +1,16 @@
+package murat.utilities;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTestReport {
+    @BeforeMethod
+    public void setup(){
+
+    }
+    @AfterMethod
+    public void teardown(){
+
+    }
+
+}
