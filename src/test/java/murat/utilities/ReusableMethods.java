8252923342ca5c28ea8897508204e8a19730f4a9
QA Murat Tas <itmmt38@gmail.com>
2023-01-04 20:58:18 +0300

    wed

--- src/test/java/murat/utilities/ReusableMethods.java
+++ src/test/java/murat/utilities/ReusableMethods.java
@@ -1,13 +1,20 @@
 package murat.utilities;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ReusableMethods {
 
     public static String getScreenShot (WebDriver driver, String name){
+    // Screenshot dosya ismi için şu anki tarihi string olarak alıyoruz
+        String date = formatCurrentDate("yyyyMMddhhmmss");
+        File source = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+
 
 
     }
