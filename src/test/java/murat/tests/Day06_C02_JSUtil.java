325b6631d49278eb2e071598eb1e53c7c450ae43
QA Murat Tas <itmmt38@gmail.com>
2023-01-11 20:47:18 +0300

    wed

--- src/test/java/murat/tests/Day06_C02_JSUtil.java
+++ src/test/java/murat/tests/Day06_C02_JSUtil.java
@@ -16,15 +16,15 @@ public class Day06_C02_JSUtil extends BaseCrossBrowserTest {
         JSUtils.scrollIntoViewJS( driver.findElement(By.partialLinkText("Amazon Ignite")));
 
         // Debug amacli
-        Thread.sleep(4000);
+        Thread.sleep(2000);
 
         JSUtils.scrollAllUpByJS();
 
-        Thread.sleep(4000);
+        Thread.sleep(2000);
 
         JSUtils.flash( driver.findElement(By.id("twotabsearchtextbox")));
 
-        Thread.sleep(4000);
+        Thread.sleep(2000);
 
     }
 
